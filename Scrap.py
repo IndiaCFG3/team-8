0983fa19c3205341574115935a0427e57d922371
Aayush Fadia <aayushf@gmail.com>
2020-08-16 11:52:14 +0530

    Added JSON Serialization

--- Scrap.py
+++ Scrap.py
@@ -1,9 +1,29 @@
+from json import dumps
+
 import pandas as pd
-import numpy as np
 
-dataset = pd.read_csv("D:/Code for good/FAOSTAT.csv")
 
-new_data = dataset[dataset["Unit"]=="tonnes"]
-Extracted_data = new_data[["Area","Item Code","Year","Value"]][dataset["Item Code"]==1062][["Area","Year","Value"]]
+def get_dataframe():
+    dataset = pd.read_csv("/home/aayush/CFG/analysis/FAOSTAT_data_8-16-2020 (1).csv")
 
-Extracted_data.to_json(r"D:/Code for good/")
+    new_data = dataset[dataset["Unit"] == "tonnes"]
+    Extracted_data = new_data[["Area", "Item Code", "Year", "Value"]][dataset["Item Code"] == 1062][
+        ["Area", "Year", "Value"]]
+    # print(Extracted_data)
+    return Extracted_data
+
+
+def supply(dataframe, country):
+    data = dataframe[dataframe["Area"].eq(country)]
+    years = data["Year"].tolist()
+    values = data["Value"].tolist()
+    final_data = {
+        "years": years,
+        "values": values
+    }
+    print(dumps(final_data))
+
+
+if __name__ == '__main__':
+    all_data = get_dataframe()
+    supply(all_data, "Afghanistan")
